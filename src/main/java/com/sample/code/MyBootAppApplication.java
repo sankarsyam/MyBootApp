9092d7164c3c1bf1198fec966adaaf3d59ff626e
Syam Sankar <syam.sankar2@cognizant.com>
2017-02-06 17:26:48 +0530

    primary folders added

---
+++ src/main/java/com/sample/code/MyBootAppApplication.java
@@ -0,0 +1,12 @@
+package com.sample.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppApplication.class, args);
+	}
+}
