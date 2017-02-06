15beef43447596725246eb7777fa1cd553bbe6aa
Syam Sankar <syam.sankar2@cognizant.com>
2017-02-06 18:01:23 +0530

    Implemented service

--- src/main/java/com/sample/code/service/SampleServiceImpl.java
+++ src/main/java/com/sample/code/service/SampleServiceImpl.java
@@ -5,4 +5,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class SampleServiceImpl implements SampleService{
 
+	@Override
+	public void saveUser(String userName) throws Exception {
+		
+		System.out.println("Saved successfully");
+		
+		
+	}
+
 }
