0cb864ab1a5b2367c0146c7c48ee5adf10d54b2b
Syam Sankar <syam.sankar2@cognizant.com>
2017-02-07 17:42:14 +0530

    comment added

--- src/main/java/com/sample/code/controller/SampleController.java
+++ src/main/java/com/sample/code/controller/SampleController.java
@@ -6,7 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/user")
 public class SampleController {
-
+	/**
+	 * Hi
+	 * @return
+	 */
 	@RequestMapping(value="/mypage")
 	public String getMyPage(){
 		return "Welcome Gust!";
